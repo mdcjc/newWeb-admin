503c70128ed4654b264a7206257f44b05c3d46d6
focusJ <17665091218@163.com>
2023-07-15 18:28:57 +0800

    配置husky解决提交代码格式化

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
 // 测试提交git格式化问题
 const fn = () => {
-
-
-
-    
-	console.log("312");
-};
-fn();
-createApp(App).mount("#app");
+  console.log('312')
+}
+fn()
+createApp(App).mount('#app')
